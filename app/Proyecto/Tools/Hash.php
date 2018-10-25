18073dc94e0e1c390d5c9628088f3cae77bbf7fd
facundoi22 <facundoi22@gmail.com>
2018-10-25 20:52:28 -0300

    Agrego la carpeta tools con algunas clases nuevas

---
+++ app/Proyecto/Tools/Hash.php
@@ -0,0 +1,15 @@
+<?php
+namespace Proyecto\Tools;
+
+class Hash
+{
+    public static function encrypt($value)
+    {
+        return password_hash($value, PASSWORD_DEFAULT);
+    }
+
+    public static function verify($value, $hash)
+    {
+        return password_verify($value, $hash);
+    }
+}
