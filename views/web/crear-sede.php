69d40b4460a49738aaf805d97c736af89616c940
GonzaloVicens <vicens.gonzalo@gmail.com>
2018-12-15 17:15:24 -0300

    Estilos a crear sedes

--- views/web/crear-sede.php
+++ views/web/crear-sede.php
@@ -50,53 +50,78 @@ if (Session::has("camposError")){
 };
 
 ?>
-<main class="py-4 mb-4">
+<main class="py-4 mb-4 sedes">
     <div class="container">
         <div class="row">
             <div class="col-md-3">
             </div>
             <div class="col-md-6">
-                <h2 class="mt-5 mb-4 pfgreen">Crear <span class="font-weight-normal">Sede</span></h2>
+                <h2 class="mt-5 mb-4 pfgreen"><i class="fas fa-map-marker-alt"></i> Crear <span class="font-weight-normal">Sede</span></h2>
                 <form method='post' action='<?= App::$urlPath;?>/usuarios/crear-sede' >
                     <div class="form-group">
                        <label for="nombre">Nombre Sede</label>
-                       <input type="text" class="form-control" name="nombre" id="nombre" required placeholder="Nombre de la sede"  value="<?=$nombre?>" />
+                       <input type="text" class="form-control" name="nombre" id="nombre"  placeholder="Nombre de la sede"  value="<?=$nombre?>" />
+                        <?php
+                        if (isset($camposError['nombre'])) {
+                            echo "<p class='rta-validacion text-danger'><small>" . $camposError['nombre'] . "</small><p>";
+                        }
+                        ?>
                     </div>
                     <div class="form-group">
                        <label for="deporte">Provincia</label>
-                       <select name="provincia" id="provincia" required class="form-control" required>
+                       <select name="provincia" id="provincia"  class="form-control" >
                         <?=Sede::printOptionsProvincias($provincia)?>
                        </select>
                         <!--input type="text" class="form-control" id="nombre" aria-describedby="emailHelp" placeholder="Ingresá tu nombre"-->
                     </div>
                     <div class="form-group">
                         <label for="postal">Código Postal</label>
-                        <input type="text" class="form-control" name="postal" id="postal" required  placeholder="Código Postal" value="<?=$postal?>" />
+                        <input type="text" class="form-control" name="postal" id="postal"   placeholder="Código Postal" value="<?=$postal?>" />
+                        <?php
+                        if (isset($camposError['postal'])) {
+                            echo "<p class='rta-validacion text-danger'><small>" . $camposError['postal'] . "</small><p>";
+                        }
+                        ?>
                     </div>
                     <div class="form-group">
                         <label for="calle">Calle</label>
-                        <input type="text" class="form-control" name="calle" id="calle" required  placeholder="Calle" value="<?=$calle?>" />
+                        <input type="text" class="form-control" name="calle" id="calle"   placeholder="Calle" value="<?=$calle?>" />
+                        <?php
+                        if (isset($camposError['calle'])) {
+                            echo "<p class='rta-validacion text-danger'><small>" . $camposError['calle'] . "</small><p>";
+                        }
+                        ?>
                     </div>
                     <div class="form-group">
                         <label for="altura">Altura</label>
-                        <input type="text" class="form-control" name="altura" id="altura" required   placeholder="Altura" value="<?=$altura?>" />
+                        <input type="text" class="form-control" name="altura" id="altura"    placeholder="Altura" value="<?=$altura?>" />
+                        <?php
+                        if (isset($camposError['altura'])) {
+                            echo "<p class='rta-validacion text-danger'><small>" . $camposError['altura'] . "</small><p>";
+                        }
+                        ?>
                     </div>
                     <div class="form-group">
                         <label for="telefono">Teléfono</label>
-                        <input type="text" class="form-control" name="telefono" id="telefono" required  placeholder="Teléfono" value="<?=$telefono?>" />
+                        <input type="text" class="form-control" name="telefono" id="telefono"   placeholder="Teléfono" value="<?=$telefono?>" />
+                        <?php
+                        if (isset($camposError['telefono'])) {
+                            echo "<p class='rta-validacion text-danger'><small>" . $camposError['telefono'] . "</small><p>";
+                        }
+                        ?>
                     </div>
 
-                    <div class="col-sm-10">
+                    <div class="form-group">
                         <textarea rows='5' name="detalles" id='detalles' class="form-control" placeholder="Detalles de la sede..."><?=$detalles?></textarea>
                     </div>
 
-                    <button type="submit" class="btn btn-lg btn-outline-success">Crear</button>
-                    <a type="button" href="<?=App::$urlPath . '/usuarios/'. $usuario->getUsuarioId()?>" class="btn btn-outline-secondary" data-dismiss="modal">Cancelar</a>
+                    <button type="submit" class="btn btn-outline-success">Crear Sede</button>
+                    <a href="<?=App::$urlPath . '/usuarios/'. $usuario->getUsuarioId()?>" class="btn btn-link cancel">Cancelar</a>
                 </form>
             </div>
             <div class="col-md-3">
                 <?php
-
+                /*
                 if (isset($camposError)){
                     echo("<div class='DivErrores'><ul>");
                     foreach ($camposError as $error => $descr) {
@@ -104,6 +129,7 @@ if (Session::has("camposError")){
                     }
                     echo("</ul></div>");
                 }
+                */
                 ?>
             </div>
         </div>
