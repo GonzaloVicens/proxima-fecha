99f8ba89d697257e07457d1dc96a17be4b39b1b6
GonzaloVicens <vicens.gonzalo@gmail.com>
2018-12-15 12:07:55 -0300

    Se agregan estilos al admin PF

--- app/Proyecto/Tools/Estadisticas.php
+++ app/Proyecto/Tools/Estadisticas.php
@@ -9,10 +9,10 @@ use Proyecto\Model\Usuario;
 class Estadisticas
 {
     public static function GetEstadisticas($dias) {
-        echo "<ul><li>Usuarios Registrados: " . Usuario::GetUsuariosCreados($dias);
-        echo "</li><li>Equipos Creados: " . Equipo::GetEquiposCreados($dias);
-        echo "</li><li>Sedes Creadas: ". Sede::GetSedesCreadas($dias);
-        echo "</li><li>Torneos Creados: ". Torneo::GetTorneosCreados($dias);
+        echo "<ul class='list-group'><li class='list-group-item w-sm-100 w-50 text-secondary'><span class='font-regular-bold'><i class='fas fa-user'></i> Usuarios Registrados</span>: " . Usuario::GetUsuariosCreados($dias);
+        echo "</li><li class='list-group-item w-sm-100 w-50 text-secondary'><span class='font-regular-bold'><i class='fas fa-shield-alt'></i> Equipos Creados</span>: " . Equipo::GetEquiposCreados($dias);
+        echo "</li><li class='list-group-item w-sm-100 w-50 text-secondary'><span class='font-regular-bold'><i class='fas fa-map-marker-alt'></i> Sedes Creadas</span>: ". Sede::GetSedesCreadas($dias);
+        echo "</li><li class='list-group-item w-sm-100 w-50 text-secondary'><span class='font-regular-bold'><i class='fas fa-trophy'></i> Torneos Creados</span>: ". Torneo::GetTorneosCreados($dias);
         echo "</li></ul>";
     }
 
--- views/admin/home.php
+++ views/admin/home.php
@@ -25,11 +25,208 @@ Session::clearValue('resultadosUsuario');
 
 ?>
 
-<main class="py-4 mb-4">
+<main class="py-4 mb-4 admin">
     <div class="container">
         <div class="row">
-            <div class="col-md-10">
-                <nav class="tabs_miequipo">
+            <div class="col-md-12 my-4">
+                <a href="<?= App::$urlPath . '/adminPF/desloguear'?>" class="btn btn-outline-secondary float-right"><i class="fas fa-door-open"></i> Cerrar Sesion</a>
+                <h1 class="pfgreen">Admin PF</h1>
+            </div>
+            <div class="col-md-12">
+                <nav>
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                        <a class="nav-item nav-link pfgreen hoverVerde active" id="nav-buscar-tab" data-toggle="tab" href="#nav-buscar" role="tab" aria-controls="nav-buscar" aria-selected="true">
+                            Buscar
+                        </a>
+                        <a class="nav-item nav-link pfgreen hoverVerde" id="nav-usuarios-tab" data-toggle="tab" href="#nav-usuarios" role="tab" aria-controls="nav-usuarios" aria-selected="true">
+                            Usuarios
+                        </a>
+                        <a class="nav-item nav-link pfgreen hoverVerde" id="nav-equipos-tab" data-toggle="tab" href="#nav-equipos" role="tab" aria-controls="nav-equipos" aria-selected="false">
+                            Equipos
+                        </a>
+                        <a class="nav-item nav-link pfgreen hoverVerde" id="nav-estadisticas-tab" data-toggle="tab" href="#nav-estadisticas" role="tab" aria-controls="nav-estadisticas" aria-selected="false">
+                            Estadísticas
+                        </a>
+                    </div>
+                </nav>
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="nav-buscar" role="tabpanel" aria-labelledby="nav-buscar-tab">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h2  class="my-5 pfgreen"><i class="fas fa-search colorGris2"></i> Buscar</h2>
+                                <div class="form-container my-2">
+                                    <h3 class="my-3 h4 text-muted">Buscar Usuarios</h3>
+                                    <form action="buscar-usuario" method="POST" >
+                                        <div class="form-group">
+                                            <!--label for="buscar-nombre">Buscar Usuario por Nombre</label-->
+                                            <div class="input-group">
+                                                <input name="nombre"  class="form-control py-2 border-right-0 border" type="text" placeholder="por nombre" id="buscar-nombre">
+                                                <span class="input-group-append">
+                                                    <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <!--label for="buscar-apellido">Buscar Usuario por Apellido</label-->
+                                            <div class="input-group">
+                                                <input name="apellido"  class="form-control py-2 border-right-0 border" type="text" placeholder="por apellido" id="buscar-apellido">
+                                                <span class="input-group-append">
+                                                    <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <!--label for="buscar-id">Buscar Usuario por Id</label-->
+                                            <div class="input-group">
+                                                <input name="id"  class="form-control py-2 border-right-0 border" type="text" placeholder="por ID" id="buscar-id">
+                                                <span class="input-group-append">
+                                                    <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-outline-success">Buscar</button>
+                                    </form>
+                                </div>
+                                <div class="form-container my-4">
+                                    <h3 class="my-3 h4 text-muted">Buscar Equipos</h3>
+                                    <form action="buscar-equipo" method="POST" >
+                                        <div class="form-group">
+                                            <!--label for="buscar-nombre">Buscar Equipo por Nombre</label-->
+                                            <div class="input-group">
+                                                <input name="nombre"  class="form-control py-2 border-right-0 border" type="text" placeholder="por nombre" id="buscar-nombre">
+                                                <span class="input-group-append">
+                                                    <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <!--label for="buscar-id">Buscar Equipo por Id</label-->
+                                            <div class="input-group">
+                                                <input name="id"  class="form-control py-2 border-right-0 border" type="text" placeholder="por ID" id="buscar-id">
+                                                <span class="input-group-append">
+                                                    <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-outline-success">Buscar</button>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <h2  class="mt-5 mb-4 pfgreen">Resultados</h2>
+                                <?php
+                                if (isset($resultadosUsuario) && !empty($resultadosUsuario[0]) ) {
+                                    Usuario::imprimirUsuariosPorArray($resultadosUsuario);
+                                } else if (isset($resultadosEquipo) && !empty($resultadosEquipo[0]) ) {
+                                    Equipo::imprimirEquiposPorArray($resultadosEquipo);
+                                } else {
+                                    echo "<i class='fontSize6rem colorGris1 fas fa-search mb-3 text-center d-block'></i> <br>";
+                                    echo "<p class='colorGris1  h2 text-center font-weight-normal'>No se han realizado búsquedas</p>";
+                                }?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade show" id="nav-usuarios" role="tabpanel" aria-labelledby="nav-usuarios-tab">
+                        <div>
+                            <h2  class="my-5 pfgreen"><i class="fas fa-user colorGris2"></i> Listado de Usuarios</h2>
+                            <div>
+                                <?= Usuario::imprimirUsuariosEnTabla(); ?>
+                                <!--form action="buscar-usuario" method="POST" >
+                                    <div class="form-group">
+                                        <label for="buscar-nombre">Buscar Usuario por Nombre</label>
+                                        <div class="input-group">
+                                            <input name="nombre"  class="form-control py-2 border-right-0 border" type="text" placeholder="Nombre" id="buscar-nombre">
+                                            <span class="input-group-append">
+                                                <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="buscar-apellido">Buscar Usuario por Apellido</label>
+                                        <div class="input-group">
+                                            <input name="apellido"  class="form-control py-2 border-right-0 border" type="text" placeholder="Apellido" id="buscar-apellido">
+                                            <span class="input-group-append">
+                                                <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="buscar-id">Buscar Usuario por Id</label>
+                                        <div class="input-group">
+                                            <input name="id"  class="form-control py-2 border-right-0 border" type="text" placeholder="ID" id="buscar-id">
+                                            <span class="input-group-append">
+                                                <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-outline-success">Buscar</button>
+                                </form-->
+                            </div>
+                            <?php
+                            /*
+                            if (isset($resultadosUsuario) && !empty($resultadosUsuario[0]) ) {
+                                Usuario::imprimirUsuariosPorArray($resultadosUsuario);
+                            } else {
+                                Usuario::imprimirUsuariosEnTabla();
+                            }
+                            */
+                            ?>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="nav-equipos" role="tabpanel" aria-labelledby="nav-equipos-tab">
+                        <div>
+                            <h2  class="my-5 pfgreen"><i class="fas fa-shield-alt colorGris2"></i> Listado de Equipos</h2>
+                            <div>
+                                <?= Equipo::imprimirEquiposEnTabla(); ?>
+                                <!--form action="buscar-equipo" method="POST" >
+                                    <div class="form-group">
+                                        <label for="buscar-nombre">Buscar Equipo por Nombre</label>
+                                        <div class="input-group">
+                                            <input name="nombre"  class="form-control py-2 border-right-0 border" type="text" placeholder="Nombre" id="buscar-nombre">
+                                            <span class="input-group-append">
+                                                <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="buscar-id">Buscar Equipo por Id</label>
+                                        <div class="input-group">
+                                            <input name="id"  class="form-control py-2 border-right-0 border" type="text" placeholder="ID" id="buscar-id">
+                                            <span class="input-group-append">
+                                                <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-outline-success">Buscar</button>
+                                </form-->
+                            </div>
+                            <?php
+                            /*
+                            if (isset($resultadosEquipo) && !empty($resultadosEquipo[0]) ) {
+                                Equipo::imprimirEquiposPorArray($resultadosEquipo);
+                            } else {
+                                Equipo::imprimirEquiposEnTabla();
+                            }
+                            */
+                            ?>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="nav-estadisticas" role="tabpanel" aria-labelledby="nav-estadisticas-tab">
+                        <h2  class="my-5 pfgreen"><i class="fas fa-chart-bar colorGris2"></i> Estadísticas</h2>
+                        <ul class="list-unstyled ">
+                            <li><h3 class="h4 colorGris2 my-4"><i class="fas fa-calendar-alt verde3 h5"></i> Última Semana</h3>
+                                <?php Estadisticas::GetEstadisticas("7")?>
+                            </li>
+                            <li><li><h3 class="h4 colorGris2 my-4"><i class="fas fa-calendar-alt verde3 h5"></i> Último Mes</h3>
+                                <?php Estadisticas::GetEstadisticas("30")?>
+                            </li>
+                            <li><li><h3 class="h4 colorGris2 my-4"><i class="fas fa-calendar-alt verde3 h5"></i> Último Año</h3>
+                                <?php Estadisticas::GetEstadisticas("365")?>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!--nav class="tabs_admin">
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
                         <a class="nav-item nav-link pfgreen hoverVerde active" id="nav-equipos-tab" data-toggle="tab" href="#nav-equipos" role="tab" aria-controls="nav-equipos" aria-selected="true">Equipos</a>
                         <a class="nav-item nav-link pfgreen hoverVerde " id="nav-usuarios-tab" data-toggle="tab" href="#nav-usuarios" role="tab" aria-controls="nav-usuarios" aria-selected="false">Usuarios</a>
@@ -73,7 +270,7 @@ Session::clearValue('resultadosUsuario');
                     </div>
                 </div>
                 <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="nav-equipos" role="tabpanel" aria-labelledby="nav-equipos-tab">
+                    <div class="tab-pane fade show active" id="nav-usuarios" role="tabpanel" aria-labelledby="nav-usuarios-tab">
                         <div>
                             <h2  class="my-5 pfgreen">Listado de Usuarios</h2>
                             <div>
@@ -132,10 +329,7 @@ Session::clearValue('resultadosUsuario');
                             </li>
                         </ul>
                     </div>
-                </div>
-            </div>
-            <div class="col-md-2">
-                <a href="<?= App::$urlPath . '/adminPF/desloguear'?>" class="btn btn-outline-secondary float-right"><i class="fas fa-door-open"></i> Cerrar Sesion</a>
+                </div-->
             </div>
         </div>
     </div>
